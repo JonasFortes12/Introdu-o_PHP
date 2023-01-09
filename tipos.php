83232ab5ae14a2f299275fe1b9ed6d75e73a388e
Jonas Fortes <jonascforte@hotmail.com>
2023-01-09 02:04:38 -0300

    Tipos primitivos de dados

---
+++ tipos.php
@@ -0,0 +1,17 @@
+<?php
+/* -- Tipos Promitivos de Dados --
+
+    PHP é dinâmicamente tipado.
+*/
+
+$nome = "Jonas Fortes"; //string
+$idade = 21; // inteiro
+$salario = 10302.50;
+$divisao = 10/3; // boolean ou float
+$boolean = true; //boelano
+
+
+
+echo gettype($boolean);//pega o tipo de dodo da variáveil
+
+
