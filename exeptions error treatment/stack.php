ac778a9665ca24a3b681913df158b572654a654e
Jonas Fortes <jonascforte@hotmail.com>
2023-02-18 14:32:55 -0300

    Execution stack

---
+++ exeptions error treatment/stack.php
@@ -0,0 +1,19 @@
+<?php
+
+function func1()
+{
+    echo 'Starting func1 routine' . PHP_EOL;
+    func2();
+    echo 'Ending func1 routine' . PHP_EOL;
+}
+
+function func2()
+{
+    echo 'Starting func2 routine' . PHP_EOL;
+    for ($i=0; $i<5; $i++) echo $i . PHP_EOL;
+    echo 'Ending func2 routine' . PHP_EOL;
+}
+
+echo 'Starting main routine' . PHP_EOL;
+func1();
+echo 'Ending main routine' . PHP_EOL;
