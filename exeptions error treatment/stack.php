3a05d3122246a13957091540ecc656d236218d52
Jonas Fortes <jonascforte@hotmail.com>
2023-02-18 15:49:41 -0300

    Try Catch

--- exeptions error treatment/stack.php
+++ exeptions error treatment/stack.php
@@ -3,6 +3,22 @@
 function func1()
 {
     echo 'Starting func1 routine' . PHP_EOL;
+
+    //Fatal Error:
+    try {
+        intdiv(7, 0);
+    }catch (Error $error){
+        echo "An Error detected: " . PHP_EOL . $error . PHP_EOL;
+    }
+
+    //Runtime Exception:
+    try {
+        $array = new SplFixedArray(2);
+        $array[4] = 'new element';
+    }catch (RuntimeException $error){
+        echo "A RuntimeException detected: " . PHP_EOL . $error . PHP_EOL;
+    }
+
     func2();
     echo 'Ending func1 routine' . PHP_EOL;
 }
@@ -10,7 +26,7 @@ function func1()
 function func2()
 {
     echo 'Starting func2 routine' . PHP_EOL;
-    for ($i=0; $i<5; $i++) echo $i . PHP_EOL;
+    for ($i = 0; $i < 5; $i++) echo $i . PHP_EOL;
     echo 'Ending func2 routine' . PHP_EOL;
 }
 
